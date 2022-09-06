9d966bf8428e0fe9fea67b3d1532d3fd2b617a11
dahye1013 <alrean@naver.com>
2022-09-07 01:15:11 +0900

    chore: 누락된 emotion compiler 세팅 추가
    referece - https://nextjs.org/docs/advanced-features/compiler

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+    reactStrictMode: true,
+    swcMinify: true,
+    compiler: {
+        emotions: true,
+    },
 };
 
 module.exports = nextConfig;
