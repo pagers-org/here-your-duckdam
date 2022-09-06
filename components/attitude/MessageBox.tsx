3499cd0ff403557fcdc2065eda6141e83ff4b120
dahye1013 <alrean@naver.com>
2022-09-06 12:23:44 +0900

    feat: atitude 페이지 구현

---
+++ components/attitude/MessageBox.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+
+const MessageBox = (props: { children: React.ReactNode }) => {
+    return (
+        <Wrapper>{props.children}</Wrapper>
+    )
+}
+
+export default MessageBox
+
+const Wrapper = styled.section`
+    height: 50%;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-end;
+    box-sizing: border-box;
+    padding: 2rem;
+
+`
