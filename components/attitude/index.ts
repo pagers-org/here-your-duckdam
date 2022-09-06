0ce343b8fb9948074dcb52edef329e4311b5a7db
dahye1013 <alrean@naver.com>
2022-09-07 00:51:39 +0900

    feat: splash logo 컴포넌트

--- components/attitude/index.ts
+++ components/attitude/index.ts
@@ -2,3 +2,4 @@ export { default as AttitudeRange } from './AttitudeRange';
 export { default as AttitudeTitle } from './AttitudeTitle';
 export { default as MessageBox } from './MessageBox';
 export { default as MessageItem } from './MessageItem';
+export { default as SplashRabbit } from './SplashRabbit';
