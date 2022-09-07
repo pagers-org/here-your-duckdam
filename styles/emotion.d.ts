3de6a5ffe06c41d204ba2472b111d25fc9355917
jlee0505 <hj960322@gmail.com>
2022-09-08 03:08:09 +0900

    refactor: 피드백 반영 - 쓸데없는 타입 삭제

--- styles/emotion.d.ts
+++ styles/emotion.d.ts
@@ -3,14 +3,6 @@ import '@emotion/react';
 declare module '@emotion/react' {
     export interface Theme {
         color: {
-            buttonYellow: Interpolation<
-                {
-                    theme?: Theme | undefined;
-                    as?: ElementType<any> | undefined;
-                } & ClassAttributes<HTMLButtonElement> &
-                    ButtonHTMLAttributes<HTMLButtonElement> &
-                    IButton & { theme: Theme }
-            >;
             buttonYellow: string;
             yellow: string;
             white: string;
