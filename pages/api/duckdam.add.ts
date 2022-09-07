320d3c24f28d4eb5b8c413775a0e2836101b89e3
dahye1013 <alrean@naver.com>
2022-09-07 11:50:48 +0900

    fix: lint simple import 에러 수정

--- pages/api/duckdam.add.ts
+++ pages/api/duckdam.add.ts
@@ -1,28 +1,27 @@
-import { db } from 'utils/firebase';
-import { collection, addDoc } from '@firebase/firestore';
+import { addDoc, collection } from '@firebase/firestore';
 import { NextApiRequest, NextApiResponse } from 'next';
+// eslint-disable-next-line import/no-unresolved
+import { db } from 'utils/firebase';
 
 type ValueType = {
-  img_url: string;
-  first_word: string;
-  second_word: string;
-  third_word: string;
+    img_url: string;
+    first_word: string;
+    second_word: string;
+    third_word: string;
 };
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'POST') {
-    console.log(req.body);
-    const { img_url, first_word, second_word, third_word } = req.body;
+    if (req.method === 'POST') {
+        const { img_url, first_word, second_word, third_word } = req.body;
 
-    const docRef = await addDoc(collection(db, 'duckdam'), {
-      img_url: img_url,
-      first_word: first_word,
-      second_word: second_word,
-      third_word: third_word,
-    });
-    console.log('Document written with ID: ', docRef.id);
-    res.status(200).json('success');
-  }
+        const docRef = await addDoc(collection(db, 'duckdam'), {
+            img_url: img_url,
+            first_word: first_word,
+            second_word: second_word,
+            third_word: third_word,
+        });
+        res.status(200).json('success');
+    }
 }
 
 export default handler;
--- utils/firebase.ts
+++ utils/firebase.ts
@@ -1,15 +1,15 @@
-import { initializeApp, getApp, getApps } from '@firebase/app';
-import { getStorage } from '@firebase/storage';
+import { getApp, getApps, initializeApp } from '@firebase/app';
 import { getFirestore } from '@firebase/firestore';
+import { getStorage } from '@firebase/storage';
 
 // 파이어베이스 Config
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FB_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FB_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FB_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FB_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FB_MESSAGE_ID,
-  appId: process.env.NEXT_PUBLIC_FB_APP_ID,
+    apiKey: process.env.NEXT_PUBLIC_FB_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FB_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FB_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FB_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FB_MESSAGE_ID,
+    appId: process.env.NEXT_PUBLIC_FB_APP_ID,
 };
 
 // 파이어베이스 앱 초기화/설정 (이미 초기화되어있으면 기존 설정 사용)
