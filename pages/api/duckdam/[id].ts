dc883685802251a752bf86e2361d6838db38d876
areumsheep <areumsheep@gmail.com>
2022-09-08 00:59:14 +0900

    fix: undefined 반환 시 상태코드 추가

--- pages/api/duckdam/[id].ts
+++ pages/api/duckdam/[id].ts
@@ -1,24 +1,17 @@
-import { collection, doc, getDoc } from "@firebase/firestore";
+import { collection, doc, getDoc } from '@firebase/firestore';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { db } from 'utils/firebase';
-
-type ValueType = {
-    img_url: string;
-    first_word: string;
-    second_word: string;
-    third_word: string;
-};
-type DataType = Record<string, ValueType>;
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'GET') {
         const { id } = req.query;
         const parseId = id as string;
 
-        const docRef = doc(db, "duckdam", parseId);
+        const docRef = doc(db, 'duckdam', parseId);
         const docSnap = await getDoc(docRef);
-        
-        res.status(200).json(docSnap.data());
+
+        if (docSnap.data()) res.status(200).json(docSnap.data());
+        else res.status(502).json('Bad Request');
     }
 }
 
