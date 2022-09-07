7cf9fc2c9e41d06209d9687c6e7af2d73345627b
dahye1013 <alrean@naver.com>
2022-09-08 08:48:17 +0900

    fix: layout align item 속성

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -28,6 +28,7 @@ const Wrapper = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
+    justify-content: space-between;
     margin: auto;
     max-width: 480px;
     width: 100%;
