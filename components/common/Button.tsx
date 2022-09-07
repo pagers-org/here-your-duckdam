602fb6f461da7b3139889dbb745ba01ff6117b93
jlee0505 <hj960322@gmail.com>
2022-09-07 16:42:33 +0900

    refactor: button 타입 좁히기

--- components/common/Button.tsx
+++ components/common/Button.tsx
@@ -1,16 +1,38 @@
 import styled from '@emotion/styled';
 import React from 'react';
+import theme, { color } from 'styles/theme';
 
-const Button = (props: { text: string }) => {
-    return <StyledButton>{props.text}</StyledButton>;
+type IButton = {
+    children: React.ReactNode;
+    onClick?: () => void;
+    backgroundColor?: typeof color[keyof typeof color];
+    padding?: string;
 };
 
-export default Button;
+const setDefaultButton = ({
+    children,
+    backgroundColor = color.buttonYellow,
+    padding = '16px',
+}: IButton) => {
+    return (
+        <Wrapper>
+            <StyledButton padding={padding} backgroundColor={backgroundColor}>
+                {children}
+            </StyledButton>
+        </Wrapper>
+    );
+};
 
-const StyledButton = styled.button`
-    width: 250px;
-    height: 50px;
-    background-color: #F3DB00;
+export default setDefaultButton;
+
+const Wrapper = styled.div`
+    width: 100%;
+    padding: 9px 0;
+`;
+
+const StyledButton = styled.button<IButton>`
+    width: 90%;
+    background-color: ${(props) => props.backgroundColor};
     border: none;
     border-radius: 10px;
     display: flex;
@@ -18,5 +40,9 @@ const StyledButton = styled.button`
     align-items: center;
     font-weight: bold;
     font-size: 1em;
-    padding: 1.5rem;
+    padding: ${(props) => props.padding};
+    position: relative;
+    right: 0;
+    left: 0;
+    margin: 0 auto;
 `;
