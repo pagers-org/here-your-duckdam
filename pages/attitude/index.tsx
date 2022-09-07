3c36195fb028062615c23b3ca9f05352db91203d
dahye1013 <alrean@naver.com>
2022-09-08 08:59:01 +0900

    feat: 예의도에 따른 랜덤 메시지 생성

--- pages/attitude/index.tsx
+++ pages/attitude/index.tsx
@@ -5,17 +5,55 @@ import {
     MessageBox,
     MessageItem,
 } from '@components/main';
+import type { PoliteKeys } from '@constant/CardMessage';
+import { word_0, word_1, word_2 } from 'constant/CardMessage';
 import Link from 'next/link';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 
-const Main = () => {
+const randomNumber = (length: number) => Math.floor(Math.random() * length);
+
+const Attitude = () => {
+    const [politeRange, setPoliteRange] = useState<PoliteKeys>('50'); // 0, 50, 100
+    const [[message1, message2], setMessage] = useState<string[]>([
+        '..?',
+        '..?',
+    ]);
+
+    const politeLevel = useMemo(() => {
+        if (politeRange > 0 && politeRange < 30) {
+            return 0;
+        }
+        if (politeRange > 33 && politeRange < 60) {
+            return 50;
+        }
+        if (politeRange > 66 && politeRange <= 100) {
+            return 50;
+        }
+        return 100;
+    }, [politeRange]);
+
+    const messageGenerator = useCallback(() => {
+        return `${word_0[randomNumber(word_0.length)]}
+        ${word_1[politeLevel][randomNumber(word_1[politeLevel].length)]} 
+        ${word_2[politeLevel][randomNumber(word_2[politeLevel].length)]}`;
+    }, [politeLevel]);
+
+    useEffect(() => {
+        return setMessage([messageGenerator(), messageGenerator()]);
+    }, [politeLevel, messageGenerator]);
+
     return (
         <>
             <AttitudeTitle text={'다른 토끼들은 이런 메시지를 뽑았어요.'} />
             <MessageBox>
-                <MessageItem>친구야, 이거 보면 치킨 사줘야해</MessageItem>
-                <MessageItem>친구야, 이거 보면 치킨 사줘야해</MessageItem>
+                <MessageItem>{message1}</MessageItem>
+                <MessageItem>{message2}</MessageItem>
+                <MessageItem>...?</MessageItem>
             </MessageBox>
-            <AttitudeRange initialValue={50} />
+            <AttitudeRange
+                initialValue={50}
+                handleRange={(range) => setPoliteRange(range)}
+            />
             <Link href="secret">
                 <Button>예절 수치 정했어!</Button>
             </Link>
@@ -23,4 +61,4 @@ const Main = () => {
     );
 };
 
-export default Main;
+export default Attitude;
