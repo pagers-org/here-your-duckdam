73b15f22eee8f225c81ec9613ae7bad663b590e4
areumsheep <areumsheep@gmail.com>
2022-09-08 00:59:39 +0900

    fix: 추가 시 document Id 반환하도록 수정

--- pages/api/duckdam/add.ts
+++ pages/api/duckdam/add.ts
@@ -2,13 +2,6 @@ import { addDoc, collection } from '@firebase/firestore';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { db } from 'utils/firebase';
 
-type ValueType = {
-    img_url: string;
-    first_word: string;
-    second_word: string;
-    third_word: string;
-};
-
 async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'POST') {
         const { img_url, first_word, second_word, third_word } = req.body;
@@ -19,7 +12,7 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
             second_word: second_word,
             third_word: third_word,
         });
-        res.status(200).json('success');
+        res.status(200).json(docRef.id);
     }
 }
 
