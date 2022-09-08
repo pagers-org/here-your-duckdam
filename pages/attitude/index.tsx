de35b9a69aa106d5b4c72d52e8f210c7faf0e151
dahye1013 <alrean@naver.com>
2022-09-09 05:59:18 +0900

    feat: 예의설정화면 버튼 button, title 컴포넌트 교체

--- pages/attitude/index.tsx
+++ pages/attitude/index.tsx
@@ -1,10 +1,6 @@
-import {
-    AttitudeRange,
-    AttitudeTitle,
-    MessageBox,
-    MessageItem,
-} from '@components/attitude';
-import { Button } from '@components/common';
+import { AttitudeRange, MessageBox, MessageItem } from '@components/attitude';
+import { Button, Title } from '@components/common';
+import { Bottom } from '@components/layout';
 import useAttitudeHooks from 'hooks/useAttitudeHooks';
 import { useRouter } from 'next/router';
 
@@ -19,7 +15,7 @@ const Attitude = () => {
 
     return (
         <>
-            <AttitudeTitle text={'다른 토끼들은 이런 메시지를 뽑았어요.'} />
+            <Title>다른 토끼들은 이런 메시지를 뽑았어요.</Title>
             <MessageBox>
                 <MessageItem>{message1}</MessageItem>
                 <MessageItem>{message2}</MessageItem>
@@ -29,7 +25,11 @@ const Attitude = () => {
                 initialValue={50}
                 handleRange={(range) => setPoliteRange(range)}
             />
-            <Button onClick={addNewDuckDamHandler}>예절 수치 정했어!</Button>
+            <Bottom>
+                <Button onClick={addNewDuckDamHandler}>
+                    예절 수치 정했어!
+                </Button>
+            </Bottom>
         </>
     );
 };
