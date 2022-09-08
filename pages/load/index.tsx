875a886808a1897157d103a8f966b52bb063bbc3
dahye1013 <alrean@naver.com>
2022-09-08 10:54:18 +0900

    feat: 랜덤 덕담 생성 임시처리 - image 매핑 처리 미완료

--- pages/load/index.tsx
+++ pages/load/index.tsx
@@ -1,5 +1,63 @@
+import { Logo, Title } from '@components/common';
+import { useRouter } from 'next/router';
+import React, { useCallback, useEffect } from 'react';
+import type { PoliteKey } from 'shared/types/DuckDam';
+import { randomNewDuckDam } from 'shared/utils/duckdamGenerator';
+
 const Load = () => {
-    return <div>Load</div>;
+    const router = useRouter();
+
+    const addNewDuckDamHandler = useCallback(
+        async (politeLevel: PoliteKey) => {
+            const newDuckDam = {
+                // TODO: politeLevel 기준으로 FireBase ImageURL 매핑 작업 진행 필요함.
+                img_url: '',
+                ...randomNewDuckDam(politeLevel),
+            };
+            const response = await fetch('/api/duckdam/add', {
+                method: 'POST',
+                body: JSON.stringify(newDuckDam),
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+
+            const id = await response.json();
+
+            setTimeout(() => {
+                router.push(`/secret/?result${id}`);
+            }, 3000);
+        },
+        [router]
+    );
+
+    useEffect(() => {
+        const politeLevel = router.query.politeLevel;
+
+        const hasPoliteLevel = (
+            politeLevel: unknown
+        ): politeLevel is PoliteKey => {
+            return politeLevel !== undefined && typeof politeLevel === 'string';
+        };
+
+        if (hasPoliteLevel(politeLevel)) {
+            addNewDuckDamHandler(politeLevel);
+            return;
+        }
+
+        if (!hasPoliteLevel(politeLevel)) {
+            alert('예의도가 선택되지 않았어요!');
+            router.push('/');
+            return;
+        }
+    }, [router.query.politeLevel, router, addNewDuckDamHandler]);
+
+    return (
+        <>
+            <Logo />
+            <Title>토끼가 덕담 고르는 중</Title>
+        </>
+    );
 };
 
 export default Load;
