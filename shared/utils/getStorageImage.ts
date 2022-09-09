49f482503e0d4fee4350e84105dedf8721b63088
areumsheep <areumsheep@gmail.com>
2022-09-10 00:06:47 +0900

    fix: 첫번째 덕담 랜덤으로 생성하여 open graph 이미지 설정

--- shared/utils/getStorageImage.ts
+++ shared/utils/getStorageImage.ts
@@ -1,6 +1,5 @@
 import { randomNumber } from './randomNumber';
 
-export const getStorageImage = () => {
-    const index = randomNumber(4);
+export const getStorageImage = (index: number) => {
     return `https://firebasestorage.googleapis.com/v0/b/here-your-duckdam.appspot.com/o/images%2F${index}.png?alt=media`;
 };
