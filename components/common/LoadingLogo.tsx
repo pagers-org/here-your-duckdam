005e3440d05ee03a68035ea622cf598d14780ce3
dahye1013 <alrean@naver.com>
2022-09-10 00:48:30 +0900

    fix: lint error 수정

--- components/common/LoadingLogo.tsx
+++ components/common/LoadingLogo.tsx
@@ -2,9 +2,10 @@ import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import { Keyframe, KeyframeKey } from '@styles/keyframe';
 import Image from 'next/image';
-import moon from '../../public/icons/moon.png';
-import loading from '../../public/icons/loading.gif';
 import React from 'react';
+
+import loading from '../../public/icons/loading.gif';
+import moon from '../../public/icons/moon.png';
 
 const LoadingLogo = ({ keyframe }: { keyframe?: KeyframeKey }) => {
     return (
