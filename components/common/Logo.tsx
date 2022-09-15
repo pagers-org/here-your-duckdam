9e1b40fe0042cf21d012d07332e1cfe72b2d8d88
dahye1013 <alrean@naver.com>
2022-09-15 16:37:31 +0900

    refactor: favicon 4kb 용량 ico 파일 확장자로 컨버팅

--- components/common/Logo.tsx
+++ components/common/Logo.tsx
@@ -2,14 +2,7 @@ import Image from 'next/image';
 import logo_light from 'public/icons/main-logo_light.png';
 
 const Logo = () => {
-    return (
-        <Image
-            src={logo_light}
-            min-width="300px"
-            min-height="300px"
-            alt="달토끼"
-        />
-    );
+    return <Image src={logo_light} layout="fixed" alt="달토끼" />;
 };
 
 export default Logo;
