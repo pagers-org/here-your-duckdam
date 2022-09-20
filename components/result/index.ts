a22096f2666e46dca6d00c771ee0ad13a2fa7f06
dahye1013 <alrean@naver.com>
2022-09-20 15:16:47 +0900

    remove: 미사용 컴포넌트 모듈 제거

--- components/result/index.ts
+++ components/result/index.ts
@@ -1,3 +1,2 @@
 export { default as Card } from './Card';
 export { default as LinkCopyButton } from './LinkCopyButton';
-export { shareWithKakao, shareWithOthers, shareWithTwitter } from './Links';
