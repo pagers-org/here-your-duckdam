f9502135b4296bbff0a0e93562deba2f264d88c3
Areum Yang <areumsheep@gmail.com>
2022-09-27 00:11:43 +0900

    [기능구현] 구글 애널리틱스 추가 (#55)
    * feat: 구글 애널리틱스 추가
    
    * fix: lint 에러 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,46 @@
 import Layout from '@components/layout/Layout';
 import { Global, ThemeProvider } from '@emotion/react';
+import { GA_TRACKING_ID, pageview } from '@shared/utils/gtag';
 import { global, theme } from '@styles/index';
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import Script from 'next/script';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
+    const router = useRouter();
+    useEffect(() => {
+        const handleRouteChange = (url: string) => {
+            pageview(url);
+        };
+        router.events.on('routeChangeComplete', handleRouteChange);
+
+        return () => {
+            router.events.off('routeChangeComplete', handleRouteChange);
+        };
+    }, [router.events]);
+
     return (
         <>
+            {/* Global Site Tag (gtag.js) - Google Analytics */}
+            <Script
+                strategy="afterInteractive"
+                src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+            />
+            <Script
+                id="gtag-init"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${GA_TRACKING_ID}', {
+                    page_path: window.location.pathname,
+                    });
+                `,
+                }}
+            />
             <ThemeProvider theme={theme}>
                 <Global styles={global} />
                 <Layout>
